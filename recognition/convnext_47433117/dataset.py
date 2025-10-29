a546fb78aefd7b536c82da430e4eaf34470a0208
Alex <alexwall.ace312@gmail.com>
2025-10-29 11:34:42 +1000

    Added better transforms + wrapper

--- recognition/convnext_47433117/dataset.py
+++ recognition/convnext_47433117/dataset.py
@@ -61,17 +61,50 @@ class ADNI_Loader(Dataset):
     
 
 
-#basic transform
+class Transforms:
+    """ Wrapper class holding transforms for the dataset"""
 
-basic_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5])
-])
+    basic_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5])
+    ])
+
+    train_transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),
+
+            transforms.Resize((256, 256)),
+            transforms.RandomResizedCrop(224, scale=(0.95, 1.0)),
+
+            
+            transforms.RandomAffine(
+                degrees=5,
+                translate=(0.02, 0.02),
+                scale=(0.95, 1.05),
+            ),
+
+            transforms.ColorJitter(
+                brightness=0.1, contrast=0.1
+            ),
+
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]
+            ),
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=3),  
+        transforms.Resize((224, 224)), 
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+
 
 
 if __name__ == "__main__":
-    dataset = ADNI_Loader(root="recognition/convnext_47433117/ADNI/AD_NC", split="test", transform=basic_transform)
+    dataset = ADNI_Loader(root="recognition/convnext_47433117/ADNI/AD_NC", split="test", transform=Transforms.basic_transform)
     dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
 
     for images, labels in dataloader:
