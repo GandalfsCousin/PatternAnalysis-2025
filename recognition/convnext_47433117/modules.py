de01b62137366bec510e73f7f93a2cb271316c82
Alex <alexwall.ace312@gmail.com>
2025-10-30 22:01:41 +1000

    Simplifeid model, switched to Conv2d from linear for better accuracy

--- recognition/convnext_47433117/modules.py
+++ recognition/convnext_47433117/modules.py
@@ -3,203 +3,139 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
-
 class Block(nn.Module):
     def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
-        """Follows structure of ConvNeXt block as defined in facebookresearch/ConvNeXt suplementray code with
-        custom Droppath module.
-
-        DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
-        
-        dim (int): Number of input channels
-        drop_path (float): Stochastic depth rate. Default: 0.0
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-        """
         super().__init__()
+        # 7x7 DW conv preserves spatial dims because padding=3
         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)
-
-        self.norm = LayerNorm(dim, eps=1e-6)
-
-        self.pwconv1 = nn.Linear(dim, 4 * dim) 
+        self.norm = nn.LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Conv2d(dim, 4 * dim, kernel_size=1)
         self.act = nn.GELU()
-        self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
-                                    requires_grad=True) if layer_scale_init_value > 0 else None
-
+        self.pwconv2 = nn.Conv2d(4 * dim, dim, kernel_size=1)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim, 1, 1)), requires_grad=True)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
-
     def forward(self, x):
-        """ Forward pass of the block, as definied in  facebookresearch/ConvNeXt suplementray code"""
-        input = x
+        shortcut = x
         x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = x.permute(0, 2, 3, 1)
         x = self.norm(x)
+        x = x.permute(0, 3, 1, 2)
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
         if self.gamma is not None:
             x = self.gamma * x
-        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
-
-        x = input + self.drop_path(x)
+        x = shortcut + self.drop_path(x)
         return x
-    
 
 class ConvNeXt(nn.Module):
-    """ Modified ConvNeXt from facebookresearch/ConvNeXt suplementray code, modified for our data set
-    Deafult is the Tiny model
-
-    Args:
-        in_chans (int): Number of input image channels. Default: 3
-        num_classes (int): Number of classes for classification head. Default: 1000
-        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
-        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
-        drop_path_rate (float): Stochastic depth rate. Default: 0.
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
-    """
-    def __init__(self, in_chans=3, num_classes=2, 
-                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0., 
-                 layer_scale_init_value=1e-6, head_init_scale=1.,
-                 first_conv_stride=4,
-                 ):
+    def __init__(self, in_chans=3, num_classes=2, depths=[3,3,9,3], dims=[96,192,384,768],
+                 drop_path_rate=0., layer_scale_init_value=1e-6, head_init_scale=1., classifier_dropout=0.0):
         super().__init__()
-
-        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
-        stem = nn.Sequential(
-            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=first_conv_stride),
+        self.downsample_layers = nn.ModuleList()
+        # Stem
+        self.downsample_layers.append(nn.Sequential(
+            nn.Conv2d(in_chans, dims[0], 4, stride=4),
             LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
-        )
-        self.downsample_layers.append(stem)
+        ))
+        # Downsample
         for i in range(3):
-            downsample_layer = nn.Sequential(
-                    LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
-                    nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
-            )
-            self.downsample_layers.append(downsample_layer)
-
-        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
-        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+            self.downsample_layers.append(nn.Sequential(
+                LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+                nn.Conv2d(dims[i], dims[i+1], 2, stride=2)
+            ))
+        # Stages
+        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
         cur = 0
+        self.stages = nn.ModuleList()
         for i in range(4):
-            stage = nn.Sequential(
-                *[Block(dim=dims[i], drop_path=dp_rates[cur + j], 
-                layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
-            )
+            stage = nn.Sequential(*[Block(dims[i], drop_path=dp_rates[cur+j], layer_scale_init_value=layer_scale_init_value)
+                                    for j in range(depths[i])])
             self.stages.append(stage)
             cur += depths[i]
 
-        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6)
+        self.dropout = nn.Dropout(classifier_dropout) if classifier_dropout>0 else nn.Identity()
         self.head = nn.Linear(dims[-1], num_classes)
-
         self.apply(self._init_weights)
         self.head.weight.data.mul_(head_init_scale)
         self.head.bias.data.mul_(head_init_scale)
 
-
     def _init_weights(self, m):
-        if isinstance(m, (nn.Conv2d, nn.Linear)):
+        if isinstance(m, (nn.Linear, nn.Conv2d)):
             trunc_normal_(m.weight, std=.02)
-            nn.init.constant_(m.bias, 0) # type: ignore
+            nn.init.constant_(m.bias, 0)
 
     def forward_features(self, x):
         for i in range(4):
             x = self.downsample_layers[i](x)
             x = self.stages[i](x)
-        return self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
+        return self.norm(x.mean([-2,-1]))
 
     def forward(self, x):
         x = self.forward_features(x)
+        x = self.dropout(x)
         x = self.head(x)
         return x
-    
 
 class LayerNorm(nn.Module):
-    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
-    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
-    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
-    with shape (batch_size, channels, height, width).
-    """
     def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(normalized_shape))
         self.bias = nn.Parameter(torch.zeros(normalized_shape))
         self.eps = eps
         self.data_format = data_format
-        if self.data_format not in ["channels_last", "channels_first"]:
-            raise NotImplementedError 
         self.normalized_shape = (normalized_shape, )
-    
+
     def forward(self, x):
-        if self.data_format == "channels_last":
+        if self.data_format=="channels_last":
             return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-        elif self.data_format == "channels_first":
+        else:
             u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)
-            x = (x - u) / torch.sqrt(s + self.eps)
-            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            s = (x-u).pow(2).mean(1, keepdim=True)
+            x = (x-u)/torch.sqrt(s+self.eps)
+            x = self.weight[:,None,None]*x + self.bias[:,None,None]
             return x
-        
 
 class DropPath(nn.Module):
-    """Custom DropPath moduel to replace timm
-    Drop paths (Stochastic Depth) per sample."""
     def __init__(self, drop_prob=0.0):
         super().__init__()
         self.drop_prob = drop_prob
 
     def forward(self, x):
-        if self.drop_prob == 0.0 or not self.training:
+        if self.drop_prob==0.0 or not self.training:
             return x
         keep_prob = 1 - self.drop_prob
-        shape = (x.shape[0],) + (1,) * (x.ndim - 1)
-        random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+        shape = (x.shape[0],)+(1,)*(x.ndim-1)
+        random_tensor = keep_prob + torch.rand(shape, device=x.device)
         random_tensor.floor_()
-        return x.div(keep_prob) * random_tensor
-    
-
+        return x.div(keep_prob)*random_tensor
 
 def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
-    """
-    Custom trunc_normal_ function to replace timm
-    Fills the input Tensor with values drawn from a truncated normal distribution.
-    """
-    def norm_cdf(x):
-        return (1. + math.erf(x / math.sqrt(2.))) / 2. #normal cumulative distribution
-
+    def norm_cdf(x): return (1.+math.erf(x/math.sqrt(2.)))/2
     with torch.no_grad():
-        l = norm_cdf((a - mean) / std)
-        u = norm_cdf((b - mean) / std)
-
-        tensor.uniform_(2 * l - 1, 2 * u - 1)
+        l = norm_cdf((a-mean)/std)
+        u = norm_cdf((b-mean)/std)
+        tensor.uniform_(2*l-1, 2*u-1)
         tensor.erfinv_()
-
-        tensor.mul_(std * math.sqrt(2.))
+        tensor.mul_(std*math.sqrt(2.))
         tensor.add_(mean)
-        tensor.clamp_(min=a, max=b)
-        return tensor
+        tensor.clamp_(min=a,max=b)
+    return tensor
 
 
 
+def custom_small(drop_path_rate=0.15, layer_scale_init_value=1e-6, head_init_scale=1, classifier_dropout=0.3):
+    return ConvNeXt(depths=[3,3,27,3], dims=[96,192,384,768],
+                    drop_path_rate=drop_path_rate,
+                    layer_scale_init_value=layer_scale_init_value,
+                    head_init_scale=head_init_scale,
+                    classifier_dropout=classifier_dropout)
 
-def small_model(drop_path_rate=0.15, layer_scale_init_value=1e-6, head_init_scale=1):
-    """ Creates a Small ConvNeXt model, as is better for our dataset."""
-    return ConvNeXt(
-        depths=[3, 3, 27, 3], 
-        dims=[96, 192, 384, 768],
-        drop_path_rate=drop_path_rate, 
-        layer_scale_init_value=layer_scale_init_value, 
-        head_init_scale=head_init_scale,
-        )   
-
-
-def custom_model(drop_path_rate=0.15, layer_scale_init_value=1e-6, head_init_scale=1):
-    """ Creates a custom ConvNeXt model, as is better for our dataset."""
-    return ConvNeXt(
-        depths=[3, 3, 9, 3],
-        dims=[96, 192, 384, 768],
-        drop_path_rate=drop_path_rate, 
-        layer_scale_init_value=layer_scale_init_value, 
-        head_init_scale=head_init_scale,
-        )   
+def custom_model(drop_path_rate=0.15, layer_scale_init_value=1e-6, head_init_scale=1, classifier_dropout=0.3):
+    return ConvNeXt(depths=[3,3,9,3], dims=[96,192,384,768],
+                    drop_path_rate=drop_path_rate,
+                    layer_scale_init_value=layer_scale_init_value,
+                    head_init_scale=head_init_scale,
+                    classifier_dropout=classifier_dropout)
